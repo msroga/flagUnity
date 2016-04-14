0f1c42c6ab124f21445e909401fd9ecac227e7d8
Myo <marsroga@gmail.com>
2016-04-14 14:12:02 +0200

    first

---
+++ Assets/WindConrol.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindConrol : MonoBehaviour {
+
+    private float x;
+
+    private float z;
+	void Start () {
+        x = 8;
+        z = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.GetComponent<Cloth>().externalAcceleration = new Vector3(x, 0, z);
+    }
+
+    public void setZ(float newZ)
+    {
+        z = newZ;
+    }
+
+    public void setX(float newX)
+    {
+        x = newX;
+    }
+}
